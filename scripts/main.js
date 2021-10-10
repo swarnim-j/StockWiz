2217b9a8a7321b5509f107cbbd6addbed7195d9f
Swarnim Jain <59059193+Ranger050@users.noreply.github.com>
2021-10-10 16:23:55 +0530

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -22,26 +22,7 @@ let rate_b=0.9;
 
 let n_rows = 0;
 
-let price_temp = "";
-// Set the configuration for your app
-// TODO: Replace with your project's config object
-/*var config = {
-  apiKey: "apiKey",
-  authDomain: "projectId.firebaseapp.com",
-  // For databases not in the us-central1 location, databaseURL will be of the
-  // form https://[databaseName].[region].firebasedatabase.app.
-  // For example, https://your-database-123.europe-west1.firebasedatabase.app
-  databaseURL: "https://databaseName.firebaseio.com",
-  storageBucket: "bucket.appspot.com"
-  };
-  firebase.initializeApp(config);
-  
-  // Get a reference to the database service
-  var database = firebase.database();*/
-
-
-
-function fetchDataFromTicker() {
+function getStockData() {
 
   apiTicker = document.getElementById('input_stockname').value;
   if (apiTicker.length == 0) {
